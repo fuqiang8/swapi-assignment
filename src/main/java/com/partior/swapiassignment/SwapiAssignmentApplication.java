bffc68a4c431b321360fb6d4d274ef39d6c27e8d
Fu Qiang Wong <fuqiang20@gmail.com>
2023-12-23 14:55:50 +0800

    Base made from Spring initializr

---
+++ src/main/java/com/partior/swapiassignment/SwapiAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.partior.swapiassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapiAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partior/swapiassignment/SwapiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partior.swapiassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapiAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
